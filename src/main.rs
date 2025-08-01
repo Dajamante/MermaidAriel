39dee2706b24450a9fed44bb85050a245bc95ed2
The Macssata <aissata@fastmail.com>
2025-08-01 11:08:09 +0200

    nrf

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,29 @@
 #![no_main]
 #![no_std]
-use ariel_os::debug::log::info;
+use ariel_os::debug::log::{debug, info};
 
 use ariel_os::{
     gpio::{Input, Level, Output, Pull},
     hal::peripherals,
     time::Timer,
 };
+#[cfg(context = "espressif-esp32-c3-lcdkit")]
 ariel_os::hal::define_peripherals!(Peripherals {
     led: GPIO7,
     btn: GPIO9,
 });
 
+#[cfg(context = "nrf52840dk")]
+ariel_os::hal::define_peripherals!(Peripherals {
+    led: P0_13,
+    btn: P0_11,
+});
+
 // TODO:
 // 1. laze-project file, config this
-// 2. find out what is going on here
-//  cargo add embassy-futures
-//     Updating crates.io index
-//       Adding embassy-futures v0.1.1 to dependencies
-//              Features:
-//              - defmt
-//              - log
-//     Updating crates.io index
-// error: failed to select a version for `esp-hal`.
-//     ... required by package `ariel-os-esp v0.2.0 (/Users/ai/Embedded/mermaidariel/build/imports/ariel-os/src/ariel-os-esp)`
-//     ... which satisfies path dependency `ariel-os-esp` (locked to 0.2.0) of package `ariel-os-hal v0.2.0 (/Users/ai/Embedded/mermaidariel/build/imports/ariel-os/src/ariel-os-hal)`
-//     ... which satisfies path dependency `ariel-os-hal` (locked to 0.2.0) of package `ariel-os-embassy v0.2.0 (/Users/ai/Embedded/mermaidariel/build/imports/ariel-os/src/ariel-os-embassy)`
-//     ... which satisfies path dependency `ariel-os-embassy` (locked to 0.2.0) of package `ariel-os v0.2.0 (/Users/ai/Embedded/mermaidariel/build/imports/ariel-os/src/ariel-os)`
-//     ... which satisfies path dependency `ariel-os` (locked to 0.2.0) of package `mermaidariel v0.1.0 (/Users/ai/Embedded/mermaidariel)`
-// versions that meet the requirements `^0.23.1` are: 0.23.1
-
-// package `ariel-os-esp` depends on `esp-hal` with feature `optfield` but `esp-hal` does not have that feature.
 #[ariel_os::task(autostart, peripherals)]
 async fn main(peripherals: Peripherals) {
+    // debug!("Inside main");
     let mut led = Output::new(peripherals.led, Level::Low);
     // let mut btn = Input::new(peripherals.btn, Pull::Up);
     let mut btn = Input::builder(peripherals.btn, Pull::Up)
@@ -44,7 +35,9 @@ async fn main(peripherals: Peripherals) {
     );
     loop {
         let _ =
-            embassy_futures::select::select(btn.wait_for_low(), Timer::after_millis(13300)).await;
+        // remove that silly select
+            // embassy_futures::poll_once(btn.wait_for_low()).await;
+        embassy_futures::select::select(btn.wait_for_low(), Timer::after_millis(13300)).await;
 
         led.toggle();
     }
